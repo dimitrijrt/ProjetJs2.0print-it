ff297d34d70ceba2d7e3591fb8bd06949235df47
dimitrijrt <dimitrijacquart@gmail.com>
2023-05-24 16:00:19 +0200

    ajout de la fonction delete img , fonction ajout img ans la modal d'ajout

--- Portfolio-architecte-sophie-bluel/FrontEnd/main.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/main.js
@@ -67,6 +67,10 @@ addEventListener('DOMContentLoaded', async (e) => {
     await fetchCategories();
     await filtrerCategories();
     
+    
+    
+   
+    
 })
 
 
@@ -78,49 +82,57 @@ const modalback = document.querySelector(".modalback2");
 const btn = document.getElementById("btnmodal");
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+const span = document.querySelector(".close");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-  modalback.style.display = "block";
-}
 
-// When the user clicks on <span> (x), close the modal
+ // récupère éléments de la last MODALE
+const modal2 = document.querySelector(".modaladd");
+const modal3 = document.querySelector(".modalback");
+
+
+// close window de la première modale
 span.onclick = function() {
   modal.style.display = "none";
   modalback.style.display = "none";
-  modal2.style.display = "none";
-  modal3.style.display = "none";
+ 
 
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
-// visualiser la modal edit onclick sur modifier
-// Get the modal
-const modal2 = document.querySelector(".modaladd");
-const modal3 = document.querySelector(".modalback");
+
+
 
 // Get the button that opens the modal
 const btn2 = document.getElementById("btnadd");
 
+// Modal1
+btn.onclick = function() {
+  modal.style.display = "block";
+  modalback.style.display = "flex";
+}
+
+// MODAL 2
 btn2.onclick = function() {
     modal.style.display = "none";
-    modal2.style.display = "block";
+    modal2.style.display = "flex";
     modal3.style.display = "block";
 }
 
 const btnback = document.querySelector(".fa-arrow-left")
+const close2 = document.getElementById("close")
+
 
 btnback.onclick = function() {
   modal.style.display = "block";
     modal2.style.display = "none";
+}
+
+// close window de la last MODAL
+close2.onclick = function() {
+  modal3.style.display = "none";
+  modal2.style.display = "none";
+  modal.style.display = "none";
+  modalback.style.display = "none";
 }
 
 
@@ -137,20 +149,54 @@ function modalWorks(data) {
       // Création d' éléments pour l'affichage des travaux
       const figure = document.createElement("figure");
       const deleteIcon = document.createElement("div");
+      deleteIcon.classList.add("delete_icon");
       deleteIcon.value = data[i].id;
       // Affichage des works
       figure.innerHTML = `
                   <img src="${data[i].imageUrl}" alt="${data[i].title}" crossorigin="anonymous">
                   <figcaption>éditer</figcaption>
               `;
+              // affiche icon delete
+         deleteIcon.innerHTML = `<i class="fa-regular fa-trash-can fa-2xs"></i>
+`;
 
               const galleryEl = document.querySelector(".modal_gallery");
               galleryEl.appendChild(figure);
+              figure.appendChild(deleteIcon);
 
 
               const id = data[i].id;
 
+
+
+              // FONCTION delete IMG
+              async function deleteElement(e) {
+           
+                let id = deleteIcon.value;
+          
+              let response = await fetch("http://localhost:5678/api/works" + "/" + id, {
+                      method: "DELETE",
+                      headers: {
+                        "Content-Type": "application/Json",
+                        Authorization: "Bearer " + localStorage.getItem("accessToken"),
+                      },
+                    });
+                 
+                    
+                    if (response.status === 200 || 204) {
+                      udateWorks();
+                    } else if (response.status === 401 || 400) {
+                      console.log("erreur");
+                    }
+                  }
+
+                  deleteIcon.addEventListener("click", deleteElement);
+
     }
+    
+
+
+
 }
 
   // recup token de l'admin
@@ -182,33 +228,122 @@ const log = document.querySelector('.login');
   }
 
 
-  // fonction ajout
 
-  async function addWork() {
-    // e.preventDefault();
+  
+
+
+
+// AFFICHER IMG dans la modal d'ajout
+
+
+const inputFile = document.querySelector("#image");
+
+// Listener pour load la new img
+inputFile.addEventListener("change", loadFile);
+
+
+// afficher la new img
+function loadFile(e) {
+  e.preventDefault();
+
+
+  const reader = new FileReader();
+  reader.addEventListener("load", function () {
+    viewImg.src = reader.result;
+  });
+  // Lecture de l'image
+  reader.readAsDataURL(inputFile.files[0]);
+
+  // style de la new img
+  const picture = document.querySelector(".addphoto");
+  const label = document.querySelector(".addphoto > label");
+  const viewImg = document.createElement("img");
+  viewImg.setAttribute("id", "preview_image");
+
+  picture.appendChild(viewImg);
+
+  label.style.opacity = "0";
+  viewImg.style.position = "absolute";
+  viewImg.style.objectFit = "contain";
+  viewImg.style.opacity = "1";
+  viewImg.style.padding = "0";
+  viewImg.style.height = "140px";
+  viewImg.style.width = "390px";
+};
+
+
+
+
+
+
+// Ajouter new img à la gallerie
+
+const validerBtn = document.querySelector(".valider");
+
+
+async function addWork() {
+  
+  
+ 
+  const inputPicture = document.getElementById("image").files[0];
+  const inputTitle = document.getElementById("title").value;
+  const inputCategorie = document.getElementById("categories").value;
+  
+
+  // envoie données du formulaire new img
+  const formData = new FormData();
+  formData.append("image", inputPicture);
+  formData.append("title", inputTitle);
+  formData.append("categories", inputCategorie);
+
+  // POST request api
+
+  let response = await fetch("http://localhost:5678/api/works", {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("accesToken"),
+    },
+    body: formData,
+  });
+ 
+
+  
+  if (response.status === 200 || 204) {
+    alert('image ajoutée');
+    udateWorks();
+    modal3.style.display = "none";
+    modal2.style.display = "none";
+    modal.style.display = "none";
+    modalback.style.display= "none";
+   
     
-    // Récupération des saisies pour la création du nouvel élément
-    const inputPicture = document.getElementById("image").files[0];
-    // console.log(inputPicture);
-    const inputTitle = document.getElementById("title").value;
-    // console.log(inputTitle);
-    const inputCategorie = document.getElementById("categories").value;
-    // console.log(inputCategorie);
-  
-    // Construction du formData à envoyer
-    const formData = new FormData();
-    formData.append("image", inputPicture);
-    formData.append("title", inputTitle);
-    formData.append("categories", inputCategorie);
-  
-    // Appel de la fonction fetch avec toutes les informations nécessaires
-    let response = await fetch("http://localhost:5678/api/works", {
-      method: "POST",
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("token"),
-      },
-      body: formData,
-    });
+    
+  } else if (response.status === 401 || 400) {
+    alert('erreur');
+    console.log("Erreur");
+  };
+};
 
-  }
+
+validerBtn.addEventListener("click", ()=> {
+  addWork();
+  console.log("TEST");
+  
+
+});
+
+function clearGallery() {
+  const modalGall = document.querySelector(".modal_gallery");
+  const gallery = document.querySelector(".gallery");
+  modalGall.innerHTML = "";
+  gallery.innerHTML = "";
+};
+
+
+function udateWorks() {
+  clearGallery();
+  dataApi();
+};
+
+
 
